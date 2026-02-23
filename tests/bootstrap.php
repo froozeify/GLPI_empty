a24613071ac7f14b6628ddfd346d92c7284ff0f7
Benoit VIGNAL <git@benoit-vignal.fr>
2026-02-23 10:25:12 +0100

    Switch for phpcs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -31,9 +31,11 @@
  * -------------------------------------------------------------------------
  */
 
+$current_plugin_folder = basename(realpath(__DIR__ . '/../'));
+
 require __DIR__ . '/../../../tests/bootstrap.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (!Plugin::isPluginActive("{LNAME}")) {
-	throw new RuntimeException("Plugin {LNAME} is not active in the test database");
+if (!Plugin::isPluginActive($current_plugin_folder)) {
+	throw new RuntimeException("Plugin $current_plugin_folder is not active in the test database");
 }
